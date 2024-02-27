fe4fb20ceefb003d923cc9f9d3141e9bb389af5c
PMelon <pzx103980@student.wsb.poznan.pl>
2024-02-27 13:18:16 +0100

    Add project files.

---
+++ PPK_Projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PPK_Projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
